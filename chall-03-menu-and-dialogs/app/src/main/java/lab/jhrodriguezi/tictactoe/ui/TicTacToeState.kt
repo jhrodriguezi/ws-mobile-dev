58ac970ce3cd8f6d2befda667448b36c7b36a85c
Jhonatan Rodriguez <jsrodriguez.tech@gmail.com>
2024-11-25 23:02:20 -0500

    feat: agrega componentes para controlar dificultad y navegación

---
+++ chall-03-menu-and-dialogs/app/src/main/java/lab/jhrodriguezi/tictactoe/ui/TicTacToeState.kt
@@ -0,0 +1,37 @@
+package lab.jhrodriguezi.tictactoe.ui
+
+import kotlin.random.Random
+
+data class GameState(
+    val board: List<Cell> = List(9) { Cell.Empty },
+    val currentPlayer: Player = getRandomPlayer(),
+    val winner: Player? = null,
+    val isGameOver: Boolean = false,
+    val winningCombination: List<Int> = emptyList(),
+    val isFirstMove: Boolean = true,
+    val victories: Int = 0,
+    val defeats: Int = 0,
+    val ties: Int = 0,
+)
+
+enum class Cell {
+    Empty, X, O
+}
+
+fun getRandomPlayer(): Player {
+    return if (Random.nextBoolean()) Player.X else Player.O
+}
+
+enum class Player {
+    X, O;
+
+    fun toCell(): Cell = when (this) {
+        X -> Cell.X
+        O -> Cell.O
+    }
+
+    fun opposite(): Player = when (this) {
+        X -> O
+        O -> X
+    }
+}
